5a57ac5c76de0441b74dce0f65caee5ce497b3dc
Harsha <harshasurwase19@gmail.com>
2026-01-07 13:13:48 +0530

    some more questions realted with backtracking

---
+++ Backtracking/boundingPerm.cpp
@@ -0,0 +1,54 @@
+/*
+PS : bounding condition on permutation problem
+Where 2 consequative letters should not be there. ex AB should not be their
+In a same alphabetic order
+TC = n!
+SC = n
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// aliases
+using v = vector<string>;
+
+// global declarations
+string str = "ABC";
+vector<bool> sel = {false, false, false};
+string res;          // stores one permutation
+v ans;               // stores all permutations
+int n = str.size();
+
+// function
+void perm(int k){
+    // Base Case
+    if(k == n){
+        ans.push_back(res);
+        return;
+    }
+
+    // Try all characters
+    for(int i = 0; i < n; i++){
+        if(sel[i] == false){    
+
+            //here i will check that bounding condition
+            if(k > 0 && str[i] - res[k-1] == 1){
+                continue;
+            }
+            res[k] = str[i];   // place character
+            sel[i] = true;     // mark selected
+            perm(k + 1);
+            sel[i] = false;    // backtrack
+        }
+    }
+}
+
+int main(){
+    res.resize(n);
+    perm(0);
+
+    // display result
+    for(auto result : ans){
+        cout << result << endl;
+    }
+}
